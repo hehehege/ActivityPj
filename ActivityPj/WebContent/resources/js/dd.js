a5b7d023b12577bebd4725505e4d3e827918261a
liubb <2089853118@qq.com>
2018-12-08 12:20:05 +0800

    Revert "项目 Activity"
    This reverts commit cdd7bb6794fad05c1f259e4ef0a5d9abba35265f.

--- ActivityPj/WebContent/resources/js/dd.js
+++
@@ -1,9 +0,0 @@
-function ss() {
-		var i = document.getElementById("bbb");
-		if (document.getElementById('bbb').style.display == 'none') {
-			document.getElementById('bbb').style.display = 'block';
-		} else {
-			document.getElementById('bbb').style.display = 'none';
-		}
-
-	}
